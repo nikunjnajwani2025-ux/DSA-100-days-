12f2fc6dc4fa6087c12cf081ab6013e8fe2c3658
nikunjnajwani2025-ux <nikunjnajwani2025@gmail.com>
2026-02-08 21:38:47 +0530

    Create DAY7ii.c

---
+++ DAY7ii.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fib(int n) {
+    if (n <= 1)
+        return n;
+
+    int a = 0, b = 1, c;
+
+    for(int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+
+int main() {
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci number = %d\n", fib(n));
+
+    return 0;
+}
