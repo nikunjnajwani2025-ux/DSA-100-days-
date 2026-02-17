8d3b770d1a4d42c18defc9552b6de3c83e88f271
nikunjnajwani2025-ux <nikunjnajwani2025@gmail.com>
2026-02-17 19:22:50 +0530

    Implement spiral order matrix traversal
    Fix variable names and logic for spiral order traversal.

---
+++ DAY13i.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < r; i=i+1) {
+        for(int j = 0; j < c; j=j+1) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int t= 0, b= r - 1;
+    int l= 0, r= c - 1;
+
+    printf("Spiral order:\n");
+
+    while (top <= bottom && left <= right) {
+
+       
+        for (int j = left; j <= right; j=j+1)
+            printf("%d ", a[top][j]);
+        t=t+1;
+
+       
+        for (int i = top; i <= bottom; i=i+1)
+            printf("%d ", a[i][right]);
+        r=r-1;
+
+        
+        if (top <= bottom) {
+            for (int j = right; j >= left; j=j-1)
+                printf("%d ", a[bottom][j]);
+            b=b-1;
+        }
+
+       
+        if (left <= right) {
+            for (int i = bottom; i >= top; i=i-1)
+                printf("%d ", a[i][left]);
+            l=l+1;
+        }
+    }
+
+    return 0;
+}
