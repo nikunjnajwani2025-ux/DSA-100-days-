8df77bba9de2e20749b16bb2d755631bdd765586
nikunjnajwani2025-ux <nikunjnajwani2025@gmail.com>
2026-02-26 22:40:20 +0530

    Create DAY24i.c

---
+++ DAY24i.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, i, key;
+
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+   
+    for (i = 0; i < n; i=i+1) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    scanf("%d", &key);
+
+    
+    struct Node *curr = head, *prev = NULL;
+
+    while (curr != NULL) {
+        if (curr->data == key) {
+            if (prev == NULL) {
+               
+                head = curr->next;
+            } else {
+                prev->next = curr->next;
+            }
+            free(curr);
+            break;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
